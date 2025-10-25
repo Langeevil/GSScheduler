9b4631f63c89225a1f24fa17bf0373e1cf341c69
Gabriel Siqueira <gabrielsiqueira118@gmail.com>
2025-10-24 22:14:56 -0300

    Primeiro Commit

---
+++ src/main/java/com/gabrielsiqueira/GSScheduler/GsSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsiqueira.GSScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSchedulerApplication.class, args);
+	}
+
+}
